991c447071d49c912d15829bb43531648f69de54
beginner874 <davideyu4@gmail.com>
2024-12-17 18:18:11 +0000

    correzione esercizio

--- hello.c
+++ hello.c
@@ -20,49 +20,6 @@ int OR(int a, int B) {
     return a + B - a * B;
 }
 
-int main() {
-    // Dichiara una variabile
-    int A, B;
-
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di A (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
-    scanf("%d", &A);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore A: %d\n", A);
-
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di B (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
-    scanf("%d", &B);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore B: %d\n", B);
-
-    if (A != 0 && A != 1 && B != 0 && B != 1) {
-        printf("I valori inseriti non sono 1 o 0\n");
-    } else {
-        // Procediamo con l'esecuzione solo se il numero è 1 o 0
-        printf("I valori inseriti sono 1 o 0\n");
-
-        // Esempio di porta NOT
-        printf("Il valore di A viene trasformato da una porta NOT\n");
-        int notA = NOT(A);
-        printf("Il valore di uscita della porta NOT è: %d\n", notA);
-
-        // Esempio di porta AND
-        printf("I valori di A e B vengono trasformati da una porta AND\n");
-        int andAB = AND(A, B);
-        printf("Il valore di uscita della porta AND è: %d\n", andAB);
-
-        // Esempio di porta OR
-        printf("I valori di A e B vengono trasformati da una porta OR\n");
-        int orAB = OR(A, B);
-        printf("Il valore di uscita della porta OR è: %d\n", orAB);
-    }
-
-    printf("\n\n");
-}
-
 int LAB03 () {
     int A, B;
     printf("Inserisci il valore di A (0 o 1):\n");
