0b225f6b9a00cebb3098ffbc903326ac0791adc4
beginner874 <davideyu4@gmail.com>
2024-12-17 18:16:00 +0000

    prova esercizio 1.0 (LAB03)

--- hello.c
+++ hello.c
@@ -63,3 +63,29 @@ int main() {
     printf("\n\n");
 }
 
+int LAB03 () {
+    int A, B;
+    printf("Inserisci il valore di A (0 o 1):\n");
+    scanf("%d", &A);
+    printf("Hai inserito il valore A: %d\n", A);
+    printf("Inserisci il valore di B (0 o 1):\n");
+    scanf("%d", &B);
+    printf("Hai inserito il valore B: %d\n", B);
+
+    if (A != 0 && A != 1 && B != 0 && B != 1) {
+        printf("I valori inseriti non sono 1 o 0\n");
+    } else {
+        
+        printf("I valori inseriti sono 1 o 0\n");
+
+        printf("i valori di A e B vengono trasferiti nella porta OR di uscita viene nominata P1");
+        int P1 = OR(A,B);
+        printf("i valori di A e P1 vengono trasferiti nella porta and di uscita viene nominata P2");
+        int P2 = AND(P1,A);
+        printf("il valore di P2 viene trasferita nella porta not di uscita viene nominata Q");
+        int Q = NOT(P2);
+        printf("Il valore finale ovvero di Q e: %d\n", Q);
+        
+    }
+    printf("\n\n");
+}
